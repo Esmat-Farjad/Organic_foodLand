9b1d3f531e1e47d8f085f561c63e662e09c261ec
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2024-04-30 22:00:19 +0430

    animation created for description section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,10 @@
 // self executing function
 (function() {
-    document.querySelector(".heading-content").classList.add("apply-animation");
-    if(document.scrollingElement.scrollTop){
-        document.querySelectorAll(".card").classList.add("apply-animation");
-        console.log("scrolling event");
-    }
+    document.querySelector(".heading-content").classList.add("apply-animation-left");
 })();
 window.addEventListener('scroll', function(event) {
     this.document.querySelectorAll(".card").forEach(element => {
-        element.classList.add("apply-animation");
+        element.classList.add("slide-up-animation");
     });
+    this.document.querySelector(".description-section").classList.add("slide-up-animation");
    });
