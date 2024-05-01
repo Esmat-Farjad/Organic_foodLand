ca78a538b63eec78f1f0730329307f0bc4c4dbf5
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2024-05-01 13:56:39 +0430

    fade animation applied on scrolling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,16 @@
 // self executing function
 (function() {
     document.querySelector(".heading-content").classList.add("apply-animation-left");
+
 })();
 window.addEventListener('scroll', function(event) {
     this.document.querySelectorAll(".card").forEach(element => {
         element.classList.add("slide-up-animation");
     });
-    this.document.querySelector(".description-section").classList.add("slide-up-animation");
-   });
+    let scroll_element = this.document.querySelector(".description-section");
+    let position = scroll_element.getBoundingClientRect();
+    // checking
+    if(position.top >= 0 && position.bottom <= window.innerHeight){
+        document.getElementById("text-body").style.animation="fadeIn 2s ease-in-out 1";
+    }
+});
