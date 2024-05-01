9c478658266f1590720c2662cb158163c498cf74
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2024-05-01 14:08:26 +0430

    scroll editing...

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,12 +4,14 @@
 
 })();
 window.addEventListener('scroll', function(event) {
-    this.document.querySelectorAll(".card").forEach(element => {
-        element.classList.add("slide-up-animation");
-    });
+    let cardElement = document.querySelector(".card-section");
+    let cardPosition = cardElement.getBoundingClientRect();
     let scroll_element = this.document.querySelector(".description-section");
     let position = scroll_element.getBoundingClientRect();
     // checking
+    if(cardPosition.top >= 0 && cardPosition.bottom <= window.innerHeight) {
+        document.getElementById("card").style.animation="fadeIn 2s ease-in-out 1";
+    }
     if(position.top >= 0 && position.bottom <= window.innerHeight){
         document.getElementById("text-body").style.animation="fadeIn 2s ease-in-out 1";
     }
