a3d9e4c08a68f691c9722c2c77fa38babf3406df
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2024-05-02 11:47:05 +0430

    animation completed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,17 +4,16 @@
 
 })();
 window.addEventListener('scroll', function(event) {
-    let cardElement = document.querySelector(".card-section");
-    let cardPosition = cardElement.getBoundingClientRect();
+    let location = document.querySelector(".location");
+    let location_position = location.getBoundingClientRect();
     let scroll_element = this.document.querySelector(".description-section");
     let position = scroll_element.getBoundingClientRect();
     // checking
-    if(cardPosition.top >= 0 && cardPosition.bottom <= window.innerHeight) {
-       document.querySelectorAll(".card").forEach(element => {
-            element.classList.add("slide-up-animation");
-       });
-    }
     if(position.top >= 0 && position.bottom <= window.innerHeight){
         document.querySelector(".description-section").classList.add("slide-left-animation");
+    }
+    if(location_position.top >= 0 && location_position.bottom <= window.innerHeight){
+        document.querySelector("iframe").classList.add("slide-left-animation");
+        document.querySelector(".contact-form").classList.add("slide-right-animation");
     }
 });
